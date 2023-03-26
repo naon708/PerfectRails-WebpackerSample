a435a71d01829ef29b84d41e0b2b9b620ea7250a
naon708 <naoya_nis@icloud.com>
2023-03-26 16:03:14 +0900

    Webpackerが提供している機能を使ってloaderを追加した(bin/rails webpacker:install:erb実行)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
+// rails-erb-loaderの設定を読み込む
+const erb = require('./loaders/erb')
 
+// 配列風オブジェクトの先頭にerbを追加
+// environment.loadersはWebpackerが管理するloaderの設定リスト
+environment.loaders.prepend('erb', erb)
 module.exports = environment
