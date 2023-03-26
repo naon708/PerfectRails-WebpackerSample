0d48c105ee9177c499846f135ad11fda87cac414
naon708 <naoya_nis@icloud.com>
2023-03-26 16:23:47 +0900

    ProvidePluginというpluginをWebpackerで管理するようにした

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,11 @@
 const { environment } = require('@rails/webpacker')
 // rails-erb-loaderの設定を読み込む
 const erb = require('./loaders/erb')
+// ProvidePluginの設定を読み込む
+const provide = require('./plugins/provide')
 
 // 配列風オブジェクトの先頭にerbを追加
 // environment.loadersはWebpackerが管理するloaderの設定リスト
 environment.loaders.prepend('erb', erb)
+environment.plugins.prepend('provide', provide)
 module.exports = environment
